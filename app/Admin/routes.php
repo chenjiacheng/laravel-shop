c9d80f3fbcf04a6635e964988e611c27194a5c9e
Jason <405339556@qq.com>
2020-09-13 11:54:57 +0800

    管理后台 - 用户列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -1,15 +1,16 @@
 <?php
 
+use Encore\Admin\Facades\Admin;
 use Illuminate\Routing\Router;
+use Illuminate\Support\Facades\Route;
 
 Admin::routes();
 
 Route::group([
-    'prefix'        => config('admin.route.prefix'),
-    'namespace'     => config('admin.route.namespace'),
-    'middleware'    => config('admin.route.middleware'),
+    'prefix'     => config('admin.route.prefix'),
+    'namespace'  => config('admin.route.namespace'),
+    'middleware' => config('admin.route.middleware'),
 ], function (Router $router) {
-
-    $router->get('/', 'HomeController@index')->name('admin.home');
-
+    $router->get('/', 'HomeController@index');
+    $router->get('users', 'UsersController@index');
 });
