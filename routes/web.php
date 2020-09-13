a55c75407a8d4bc08f2b091e99a1aac858671032
Jason <405339556@qq.com>
2020-09-13 21:08:52 +0800

    管理后台 - 同意退款 - 微信支付退款

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('payment/{order}/alipay', 'PaymentController@payByAlipay')->name('payment.alipay');
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
+    Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
