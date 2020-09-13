2a57330f220eec02a9d200c88e23e67d080b16d0
Jason <405339556@qq.com>
2020-09-13 12:33:42 +0800

    管理后台 - 商品列表

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -13,4 +13,5 @@ Route::group([
 ], function (Router $router) {
     $router->get('/', 'HomeController@index');
     $router->get('users', 'UsersController@index');
+    $router->get('products', 'ProductsController@index');
 });
