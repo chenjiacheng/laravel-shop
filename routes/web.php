a0cd7fc5b2220ae8d8d7c13918a5e6d890432801
Jason <405339556@qq.com>
2020-09-13 12:56:24 +0800

    用户界面 - 商品列表

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', 'PagesController@root')->name('root');
+// Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes(['verify' => true]);
 
@@ -26,4 +26,6 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('user_addresses/{user_address}', 'UserAddressesController@edit')->name('user_addresses.edit');
     Route::put('user_addresses/{user_address}', 'UserAddressesController@update')->name('user_addresses.update');
     Route::delete('user_addresses/{user_address}', 'UserAddressesController@destroy')->name('user_addresses.destroy');
+    Route::redirect('/', '/products')->name('root');
+    Route::get('products', 'ProductsController@index')->name('products.index');
 });
