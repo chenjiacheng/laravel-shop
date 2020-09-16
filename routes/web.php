32346552bb6b105812179a5214d826779f76125c
Jason <405339556@qq.com>
2020-09-16 18:11:27 +0800

    众筹商品详情页

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
 
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
+    Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
 });
 
 Route::get('products/{product}', 'ProductsController@show')->name('products.show');
