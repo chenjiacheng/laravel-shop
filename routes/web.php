7021c6b67c153f2e5445910ab72628de0c806291
Jason <405339556@qq.com>
2020-09-17 16:40:37 +0800

    创建分期付款

--- routes/web.php
+++ routes/web.php
@@ -47,6 +47,7 @@ Route::group(['middleware' => ['auth', 'verified']], function () {
     Route::get('payment/alipay/return', 'PaymentController@alipayReturn')->name('payment.alipay.return');
     Route::get('payment/{order}/wechat', 'PaymentController@payByWechat')->name('payment.wechat');
     Route::post('payment/wechat/refund_notify', 'PaymentController@wechatRefundNotify')->name('payment.wechat.refund_notify');
+    Route::post('payment/{order}/installment', 'PaymentController@payByInstallment')->name('payment.installment');
 
     Route::get('coupon_codes/{code}', 'CouponCodesController@show')->name('coupon_codes.show');
     Route::post('crowdfunding_orders', 'OrdersController@crowdfunding')->name('crowdfunding_orders.store');
